3fcf69006cd2b0e97b71c1e5b1ab430b851b4a9e
Gaurav <gvshinde2004@gmail.com>
2025-07-10 23:55:07 +0530

    updated main

--- fake_news_detector/backend/main.py
+++ fake_news_detector/backend/main.py
@@ -1,4 +1,8 @@
 #main.py
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
+
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
